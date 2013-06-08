40f464aadab76428c967bb51c99ea2e1ff8747f8
Lars Kiesow <lkiesow@uos.de>
2013-06-09 00:07:48 +0200

    Updated readme for pyCA

--- readme.mk
+++ readme.mk
@@ -1,40 +1,19 @@
-PiCA – Matterhorn Capture Agent
+PyCA – Matterhorn Capture Agent
 ===============================
 
-**PiCA** is a [Opencast Matterhorn][1] Capture Agent based on the [Raspberry Pi][2]
-mini computer and its recently released [camera module][3]. It has a Full HD
-(1080p30) camera, a hardware h264 encoder, extensible storage (SD Card/USB
-storage), a network connector, etc. and is easily adapted to suit one's needs
-as it's running on Linux (among others).
+**PyCA** is a fully functional [Opencast Matterhorn][1] Capture Agent written
+in about 300 lines Python code. It is free software licenced under the terms of
+both the FreeBSD license and the LGPL.
 
 [1]: http://opencast.org/matterhorn
-[2]: http://raspberrypi.org
-[3]: http://www.raspberrypi.org/archives/3890 
-
-
-Features
---------
-
-* Hardware costs only about **100$**!
-* Full HD recording (1080p30)
-* Hardware h264 encoder
-* Free Open Source Software (BSD/LGPL)
-* Code easily maintainable (less than 300 lines of Python for the whole CA)
 
 
 Todo
 ----
 
-* Improve audio recording
+* Configuration
 * Confidence monitoring
 * Control page (manually start a recording, etc.)
 * Make installation easier
-	* Write a installation script
-	* Provide a operating system image
+	* Build proper Python package
 * Create daemon mode
-
-
-More Information
-----------------
-
-Visit: http://larskiesow.de/pyCA/piCA
