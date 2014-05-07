0f5dbd10713aad9e9792e1ad248ddee53c264612
Lars Kiesow <lkiesow@uos.de>
2014-05-07 16:15:30 +0200

    Updated default configuration
    Signed-off-by: Lars Kiesow <lkiesow@uos.de>

--- config.py
+++ config.py
@@ -4,10 +4,14 @@ import os
 
 CAPTURE_AGENT_NAME  = 'PyCA'
 IGNORE_TZ           = False
-ADMIN_SERVER_URL    = 'http://localhost:8080'
-ADMIN_SERVER_URL    = 'http://repo:8080'
-ADMIN_SERVER_USER   = 'digestadmin'
-ADMIN_SERVER_PASSWD = 'opencast'
+ADMIN_SERVER_URL    = 'http://example.com:8080'
+ADMIN_SERVER_USER   = 'matterhorn_system_account'
+ADMIN_SERVER_PASSWD = 'CHANGE_ME'
 UPDATE_FREQUENCY    = 60
 CAPTURE_DIR         = '%s/recordings' % os.path.dirname(os.path.abspath(__file__))
 CAPTURE_PLUGIN      = 'ffmpeg-v4l-alsa'
+
+# Setting this to true will cause the pyCA to not register itself or ingest
+# stuff to the admin server. It's useful if you want it as cbackup to another
+# CA to just get the files manually if the regular CA fails.
+BACKUP_AGENT        = False
