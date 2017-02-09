24c560402b5915a07660254f04d2891850f2c07c
Lars Kiesow <lkiesow@uos.de>
2017-02-09 19:31:19 +0100

    Add More Capture tests
    This patch adds more capture tests, ensuring that failures are handled
    gracefully.
    
    Signed-off-by: Lars Kiesow <lkiesow@uos.de>

--- tests/test_capture.py
+++ tests/test_capture.py
@@ -6,10 +6,17 @@ Tests for basic capturing
 
 import os
 import shutil
+import sys
 import tempfile
 import unittest
 
 from pyca import capture, config, db, utils
+
+if sys.version_info.major > 2:
+    try:
+        from importlib import reload
+    except ImportError:
+        from imp import reload
 
 
 class TestPycaCapture(unittest.TestCase):
@@ -19,6 +26,10 @@ class TestPycaCapture(unittest.TestCase):
     event = None
 
     def setUp(self):
+        reload(config)
+        reload(capture)
+        reload(db)
+        capture.http_request = lambda x, y=False: b'xxx'
         _, self.dbfile = tempfile.mkstemp()
         self.cadir = tempfile.mkdtemp()
         config.config()['agent']['database'] = 'sqlite:///' + self.dbfile
@@ -53,9 +64,40 @@ class TestPycaCapture(unittest.TestCase):
         shutil.rmtree(self.cadir)
 
     def test_start_capture(self):
-        # Mock some methods
-        capture.http_request = lambda x, y=False: b'xxx'
-        capture.start_capture(self.event)
+        assert capture.start_capture(self.event)
+
+    def test_start_capture_recording_command_failure(self):
+        config.config()['capture']['command'] = 'false'
+        assert not capture.start_capture(self.event)
+
+    def test_start_capture_ingest_failure(self):
+        capture.ingest = 'fail'
+        assert not capture.start_capture(self.event)
+
+    def test_register_ca(self):
+        assert capture.register_ca()
+        capture.http_request = 'fail'
+        assert not capture.register_ca()
+        config.config()['agent']['backup_mode'] = True
+        assert capture.register_ca()
+
+    def test_recording_state(self):
+        config.config()['service-capture.admin'] = ['']
+        capture.recording_state('123', 'recording')
+        capture.http_request = 'fail'
+        capture.recording_state('123', 'recording')
+        config.config()['agent']['backup_mode'] = True
+        capture.recording_state('123', 'recording')
+
+    def test_safe_start_capture(self):
+        capture.start_capture = 'fail'
+        assert not capture.safe_start_capture(1)
+        capture.start_capture = lambda x: True
+        assert capture.safe_start_capture(1)
+
+    def test_control_loop(self):
+        capture.control_loop = lambda: True
+        capture.run()
 
 
 if __name__ == '__main__':
