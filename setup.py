fa15091947c248a96243f44773e3f47ba27a53bc
Lars Kiesow <lkiesow@uos.de>
2020-07-17 22:46:31 +0200

    PyCA 3.2
    This is the release 3.2 of pyCA.
    
    Version 3.2 of pyCA introduces new functionality to monitor and manage
    the capture agent's host system by freeing disk space and providing
    access to logs.
    
    It also comes with a major overhaul of pyCA's documentation.
    
    Features
    --------
    
    - Complete overhaul of the documentation
      (682b0f0ade21ed18179334036ee51b58e1493b06)
    - Option to remove recorded files locally after they have been
      successfully uploaded (f9c7f409943f93052aaec0f2810c32a9d3ffe4b0)
    - Introduce a new optional API for system logs
      (c1c9bcede4d04f3f45bfe7f4d7d3e9ec169ae047)
      and make them available via the user interface
      (e0b11e3868bb8b435bcf9bd5f48392c07423eead)
    - Improved debugging of HTTP requests
      (6ec5d56cdbe761c7aab50e773fc57ce30aa2704b)
    - Highlight failed recordings with a warning icon
      (c343949af359559a451f8ee76574bc3202cfd053,
      d53e47cc66fda1577087de739462f58bd13af73d)
    
    Bug Fixes
    ---------
    
    - Improved routine for service endpoint updates ensuring that services
      are actually online before trying to use them e.g. for ingests
      (050bc4262f7427682a144a1495fb41f689d5b1db)
    - Fix some edge cases when starting to record which could cause the
      service to fail (baa0561ccc5aeb65745f2eac6a38df4fe6b3d864)
    - Fix test assertions (fef63d70534789a5c536d18d91ced29cd53cc895)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(filename):
 
 setup(
     name="pyca",
-    version="3.1",
+    version="3.2",
     description="Opencast Capture Agent",
     author="Lars Kiesow",
     author_email='lkiesow@uos.de',
