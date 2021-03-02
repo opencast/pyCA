c80396ef82f00e4f4844dec9736faa05e67c82fb
Lars Kiesow <lkiesow@uos.de>
2021-03-02 15:12:27 +0100

    PyCA 4.1
    This is the release 4.1 of pyCA.
    
    PyCA 4.1 improves its backup mode functionality while fixing some user
    interface bugs and introduces containers as new deployment method.
    
    Features
    --------
    
    - Allowed manual ingest via UI in backup mode (43e44a7)
    - Introduce Containerization (a5f98b6, 47f1e74)
    
    Bug Fixes
    ---------
    
    - Updated several front-end libraries
    - Fixed some documentation (2280a59)
    - Fixed re-upload of events (64fb7ca)
    - Prevent active events from being listed twice (5f909f9)
    
    Tests
    -----
    
    - Fix Coveralls deployment (64e5ca0)
    
    Development
    -----------
    
    - Separate Vue components for improved source files structure (8db49a5)
    - Droped old development UI server (596bd7c)
    
    Note About 4.0/3.3
    ------------------
    
    The same release was accidentially labeled as 3.3 at some placed as 4.0
    at others.  They are identical.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(filename):
 
 setup(
     name="pyca",
-    version="4.0",
+    version="4.1",
     description="Opencast Capture Agent",
     author="Lars Kiesow",
     author_email='lkiesow@uos.de',
