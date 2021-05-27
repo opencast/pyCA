ce6cba5a813bfb7cea58ce52e74c3cfe43568529
Lars Kiesow <lkiesow@uos.de>
2021-05-27 18:59:01 +0200

    PyCA 4.2
    This is the release 4.2 of pyCA.
    
    PyCA 4.2 is a minor bugfix release with no changes to configuration or
    recording functionality.
    
    Bug Fixes
    ---------
    
    - Updated several front-end libraries
    - Lower Required Flask Version (df4f8bf)
    - Fix potential mixed content web interface error (c35c8b8)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(filename):
 
 setup(
     name="pyca",
-    version="4.1",
+    version="4.2",
     description="Opencast Capture Agent",
     author="Lars Kiesow",
     author_email='lkiesow@uos.de',
